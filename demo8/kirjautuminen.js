257444ddcd3c1fa8d1c10f19f966b2b303033f1f
Pinja <pinja.sievanen@gmail.com>
2023-06-01 10:02:37 +0300

    Demo 8 valmis

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -1,11 +1,12 @@
 //linkitetään tiedosto index.html:ään
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut() {
-    let kirjautunut = 'true';
-if(kirjautunut === 'true') {
+if(localStorage.getItem("kirjautunut") === "kylla") {
     document.getElementById('tervetulo-teksti').textContent += localStorage.getItem("nimi");
+document.getElementById("kirjautumislomake").style.display = "none";
 }
 }
 function kirjaudu() {
     localStorage.setItem("nimi", document.getElementById("nimi").value);
+    localStorage.setItem("kirjautunut", "kylla");
 }
