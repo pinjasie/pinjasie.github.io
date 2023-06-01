0b6e441299d2655c472251250acd3139c9cf66ec
Pinja <pinja.sievanen@gmail.com>
2023-06-01 09:28:14 +0300

    Demo 7 valmis

--- demo7/kirjautuminen.js
+++ demo7/kirjautuminen.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut() {
     let kirjautunut = 'true';
 if(kirjautunut === 'true') {
-    document.getElementById('tervetulo-teksti').textContent += ' Pinja!';
+    document.getElementById('tervetulo-teksti').textContent += localStorage.getItem("nimi");
+}
 }
 function kirjaudu() {
     localStorage.setItem("nimi", document.getElementById("nimi").value);
-}
 }
